6320ceae7271e24220af8ac9ff43faf31612dd47
kongyi <jerrykongyi1997@gmail.com>
2020-08-12 00:45:54 +0800

    完成静态服务器

--- server.js
+++ server.js
@@ -24,10 +24,23 @@ var server = http.createServer(function (request, response) {
   console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
 
   response.statusCode = 200;
-  response.setHeader("Content-Type", "text/html;charset=utf-8");
+  // 默认首页
   const filePath = path === "/" ? "/index.html" : path;
+  const index = filePath.lastIndexOf(".");
+  // suffix 是后缀
+  const suffix = filePath.substring(index);
+  const fileTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+  };
+  response.setHeader(
+    "Content-Type",
+    `${fileTypes[suffix] || "text/html"};charset=utf-8`
+  );
   let content;
-
   try {
     content = fs.readFileSync(`./public${filePath}`);
   } catch (error) {
